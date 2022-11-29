335c3d8dbc70fb3cd61cb1873a089e6a4d554b1a
nyatla <nyatla39@gmail.com>
2022-11-29 22:52:34 +0900

    Modulation動作テスト。良好。

---
+++ windows/TBSKmodemCpp/Modulation/Modulation.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include "../../../src/tbskmodem/tbskmodem.h"
+#include <math.h>
+#include <memory>
+#include <vector>
+using namespace TBSKmodemCPP;
+using namespace std;
+
+
+
+
+int main()
+{
+	{
+		auto tone = make_shared<XPskSinTone>(8, 10);//10 point/cycle * 100 
+		auto preamble = make_shared<CoffPreamble>(tone);
+		auto mod = make_shared<TbskModulator>(tone, preamble);
+		std::vector<int> bits{ 0,1,0,1,0,1,0,1 };
+		auto src = std::make_shared<PyIterator<int>>(bits);
+		auto a = std::make_shared<RoStream<int>>(src);
+		auto c = mod->Modulate("Hello");
+		auto d = Functions::ToVector<double>(c);
+		FileWriter fw("./test2.wav");
+		PcmData pcm(d, 16, 8000);
+		PcmData::Dump(pcm, fw);
+	}
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
---
+++ windows/TBSKmodemCpp/PcmTest/PcmTest.cpp
@@ -0,0 +1,34 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include "../../../src/tbskmodem/tbskmodem.h"
+#include <math.h>
+using namespace TBSKmodemCPP; 
+using namespace std;
+
+int main()
+{
+	{
+		vector<double> a(16000);
+		for (auto i = 0;i < a.size();i++) {
+			a[i] = sin(i * 3.1415 * 2 / 80);
+		}
+		{
+			FileWriter fw("./test.wav");
+			PyIterator<double> b(a);
+			PcmData pcm(b, 16, 16000);
+			PcmData::Dump(pcm, fw);
+		}
+		{
+			FileReader fr("./test.wav");
+			PcmData pcm(fr);
+			FileWriter fw("./test2.wav");
+			PcmData::Dump(pcm, fw);
+
+		}
+	}
+	
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
