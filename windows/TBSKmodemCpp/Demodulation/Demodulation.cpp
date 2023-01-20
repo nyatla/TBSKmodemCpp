478b760f22039d4af5ab976d269805538453f04b
nyatla <nyatla39@gmail.com>
2023-01-20 13:33:22 +0900

    Windowsソリューションの調整

--- windows/TBSKmodemCpp/Demodulation/Demodulation.cpp
+++ windows/TBSKmodemCpp/Demodulation/Demodulation.cpp
@@ -17,18 +17,29 @@ int main()
 		auto tone = make_shared<XPskSinTone>(10, 10);//10 point/cycle * 100 
 		tone->Mul(0.5);
 		auto preamble = make_shared<CoffPreamble>(tone);
-		auto mod = make_shared<TbskModulator>(tone);
-		std::vector<int> bits{ 0,1,0,1,0,1,0,1 };
-		auto src = std::make_shared<PyIterator<int>>(bits);
+		auto mod = make_shared<TbskModulator>(tone, preamble);
+		//std::vector<int> bits{ 0,1,0,1,0,1,0,1 };
+		//auto src = std::make_shared<PyIterator<int>>(bits);
+		//auto a = std::make_shared<RoStream<int>>(src);
+		std::vector<int> bytes{};
+		auto src = std::make_shared<PyIterator<int>>(bytes);
 		auto a = std::make_shared<RoStream<int>>(src);
-		auto c = mod->ModulateAsBit(a);
-		auto d = Functions::ToVector<double>(c);
+		auto c = mod->Modulate(a);
+		//auto d = Functions::ToVector<double>(c);
+		auto d = make_shared<std::vector<double>>();
+
 		FileWriter fw("./test2.wav");
 		PcmData pcm(d, 16, 8000);
 		PcmData::Dump(pcm, fw);
 
-		auto dem = std::make_shared<TbskDemodulator>(tone);
-		auto demod_bits=dem->DemodulateAsBit(std::make_shared<PyIterator<double>>(d));
+		auto dem = std::make_shared<TbskDemodulator>(tone,preamble);
+		auto demod_bits=dem->DemodulateAsInt(std::make_shared<PyIterator<double>>(d));
+		if (demod_bits) {
+			printf("OK");
+		}
+		else {
+			printf("NG");
+		}
 		auto decoded = Functions::ToVector<int>(demod_bits);
 	}
 	_CrtDumpMemoryLeaks();
